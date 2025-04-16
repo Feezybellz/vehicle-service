168b79b7616bc76ea906333ca585019da5b74496
feezybellz <belloafeez7@gmail.com>
2025-04-16 16:50:57 +0100

    done

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import {
   Box,
   Button,
@@ -51,7 +51,7 @@ const Services = () => {
     serviceProvider: "",
   });
   const [loadingVehicles, setLoadingVehicles] = useState(false);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const effectRan = useRef(false);
 
