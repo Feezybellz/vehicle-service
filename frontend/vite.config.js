1cdebf916537c2206b20084e298c2d22782f630b
feezybellz <belloafeez7@gmail.com>
2025-04-16 19:06:34 +0100

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: process.env.PORT || 3000,
+    port: process.env.VITE_PORT || 3000,
     allowedHosts: ["localhost", "127.0.0.1", process.env.VITE_APP_URL],
   },
 });
